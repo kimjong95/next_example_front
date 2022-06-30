82151d67625ff43706dbbbcc79e5c1765f52ddb8
kimjonghyun <kimjonghyun@gimjonghyeon-ui-MacBookPro.local>
2022-06-30 16:51:02 +0900

    Pages and routing

---
+++ pages/events/add.js
@@ -0,0 +1,9 @@
+export default function AddEventPage() {
+  return (
+    <div>
+      <h1>Add Event</h1>
+      <p>This is an app to find the lastest DJ and other musical Events</p>
+      <p>Version: 1.0.0</p>
+    </div>
+  );
+}
