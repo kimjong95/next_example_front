4d6d70f20c234ce863613083ce7c5c65388dde01
kimjonghyun <kimjonghyun@gimjonghyeon-ui-MacBookPro.local>
2022-07-09 13:20:50 +0900

    single Event Page and error fix

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,13 +1,51 @@
 import Layout from "@/components/Layout";
 import { API_URL } from "@/config/index";
 import { useRouter } from "next/router";
+import styles from "@/styles/Event.module.css";
+import Link from "next/link";
+import { FaPencilAlt, FaTimes } from "react-icons/fa";
+import Image from "next/image";
 
 export default function MyEvents({ evt }) {
   const router = useRouter();
 
+  const deleteEvent = (e) => {
+    console.log(e);
+  };
+
   return (
     <Layout>
-      <h1>{evt.name}</h1>
+      <div className={styles.event}>
+        <div className={styles.controls}>
+          <Link href={`/events/edit/${evt.id}`}>
+            <a>
+              <FaPencilAlt /> Edit Event
+            </a>
+          </Link>
+          <a href="#" className={styles.delete} onClick={deleteEvent}>
+            <FaTimes /> Delete Event
+          </a>
+        </div>
+        <span>
+          {evt.date} at {evt.time}
+        </span>
+        <h1>{evt.name}</h1>
+        {evt.image && (
+          <div className={styles.image}>
+            <Image src={evt.image} width={960} height={600} />
+          </div>
+        )}
+        <h3>Performers :</h3>
+        <p>{evt.performers}</p>
+        <h3>Description :</h3>
+        <p>{evt.description}</p>
+        <h3>Venue: {evt.venue}</h3>
+        <p>{evt.address}</p>
+
+        <Link href={`/events`}>
+          <a className={styles.back}> Back</a>
+        </Link>
+      </div>
     </Layout>
   );
 }
@@ -34,7 +72,7 @@ export async function getStaticPaths() {
 // 서버에서만 실행되며 클라이언트에서는 실행되지않음
 // 빌드시 고정되는 값으로 빌드 후 변경 불가능
 // static data를 위한 fetching
-export async function getStaticProps({ query: { slug } }) {
+export async function getStaticProps({ params: { slug } }) {
   const res = await fetch(`${API_URL}/api/events/${slug}`);
   const events = await res.json();
   return {
